b570f3fe6a1e1a1096d0f7f4dff6d4fedca7a6c7
safiyabanu <safiyabanu@bsetec.com>
2025-07-10 16:25:30 +0530

    firstcommit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome Node Project\n');
+  res.end('Welcome Project\n');
 });
 
 const PORT = 3000;
