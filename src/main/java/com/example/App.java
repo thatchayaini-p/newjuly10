b982e9a328af196d9866b463a202298219d0e29f
safiyabanu <safiyabanu@bsetec.com>
2025-07-14 10:59:42 +0530

    firstcommit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,11 @@
-package com.example;
-
 public class App {
     public static void main(String[] args) {
         String s = null;
-        System.out.println(s.length()); // ❌ Bug
-
-        int unusedVar = 10; // ⚠️ Code smell
+        if (s != null) {
+            System.out.println(s.length());
+        } else {
+            System.out.println("s is null");
+        }
     }
 }
+
